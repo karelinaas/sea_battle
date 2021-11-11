96a143d35a4659e7d19c63689ea303c4106994e7
karelinaas <karelinaas95@gmail.com>
2021-11-11 20:57:57 +0300

    Channels installation

--- sea_battle/settings.py
+++ sea_battle/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -67,8 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sea_battle.wsgi.application'
-
+ASGI_APPLICATION = 'sea_battle.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
